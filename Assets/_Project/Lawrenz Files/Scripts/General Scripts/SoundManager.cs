114b0cc76b7e2ca2e4275aac07b17d6f02ce446d
Lawrenz Del Rosario <98245945+thegreatwowens@users.noreply.github.com>
2023-05-10 20:21:07 +0800

    Update Sound Manager

--- Assets/_Project/Lawrenz Files/Scripts/General Scripts/SoundManager.cs
+++ Assets/_Project/Lawrenz Files/Scripts/General Scripts/SoundManager.cs
@@ -1,11 +1,58 @@
 using UnityEngine;
 using ddr.MemoryGame.SoundClip;
-using System.Collections.Generic;
-using System.Collections;
+using System;
+using UnityEngine.Audio;
+
 public class SoundManager : MonoBehaviour
 {
-    public List<SoundClip> bGMusic;
-    public List<SoundClip> fXClips;
-    
+    public SoundClip[] bGMusic,fXClip;
+    public AudioSource bGSource,fXSource;
+    public AudioMixer mixer;
+
+      public void PlayMusic(string name,bool isLooping){
+         SoundClip sound =     Array.Find(bGMusic,x => x.name == name);
+            
+            if(sound == null){
+                
+                print ("No sound Named: "+ name+" at "+ bGMusic.ToString());
+
+            } 
+            else{
+                if(isLooping){
+                        bGSource.loop = true;
+                }else
+                    bGSource.loop = false;   
+            
+                bGSource.clip = sound.clip;
+                bGSource.Play();
+            }
+
+    }
+    public void PlaySoundFx(string name){
+        SoundClip sound =  Array.Find(fXClip,x => x.name == name);
+            
+            if(sound == null){
+                
+                print ("No sound Named: "+ name+" at "+ fXSource.ToString());
+
+            } 
+            else{
+
+                fXSource.PlayOneShot(sound.clip);
+            }
+        
+    }
+
+    public void VolumeSliderMaster (float volume){
+                bGSource.volume = volume;
+                fXSource.volume = volume;
+            
+    }
+    public void VolumeSliderSoundFx(float volume){
+           
+    }
+    public void VolumeSliderMusic(float volume){
+
+    }
 
 }
